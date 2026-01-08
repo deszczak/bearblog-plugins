16edccf344adfc9042ce0d305a94285bfd6f4060
Daniel <110098882+deshchk@users.noreply.github.com>
2026-01-08 12:24:35 +0100

    Fixing "scroll-top" continues – v3

--- plugins/scroll-top.js
+++ plugins/scroll-top.js
@@ -6,10 +6,10 @@
   const fraction = document.currentScript.getAttribute("data-fraction")
   console.log(fraction)
   document.addEventListener('scroll', () => {
-    console.log('scrolling')
     const scrolled = document.documentElement.scrollTop
     const bodySize = document.body.scrollHeight
-    btn.classList.toggle('active', scrolled > bodySize / fraction ?? 5)
+    btn.classList.toggle('active', scrolled > bodySize / (fraction ?? 5))
+    console.log(scrolled > bodySize / (fraction ?? 5))
   })
   btn.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }))
 })()
