5f1e8cd3a86acba80d99f8629e63ff0a50197c1a
Daniel <110098882+deshchk@users.noreply.github.com>
2026-01-10 19:44:29 +0100

    Added "table-of-contents" plugin and improved consistency in existing plugins

--- plugins/reading-time.js
+++ plugins/reading-time.js
@@ -7,12 +7,14 @@
     / parseInt(document.currentScript.getAttribute("data-wpm") ?? 255)
   )
   const plForms = { one: "minuta", few: "minuty", many: "minut", other: "minuty" }
-  const pr = new Intl.PluralRules('pl-PL')
+  const pr = new Intl.PluralRules("pl-PL")
   document.querySelector("main p:first-of-type").append(
     document.body.appendChild(
       Object.assign(document.createElement("i"), {
+        title: "Szacowany czas czytania:",
+        ariaLabel: "Szacowany czas czytania:",
         className: "reading-time",
-        innerHTML: time < 1 ? '<1 minuta' : `${time} ${plForms[pr.select(time)]}`
+        innerHTML: time < 1 ? "<1 minuta" : `${time} ${plForms[pr.select(time)]}`
       })
     )
   )
--- plugins/scroll-top.js
+++ plugins/scroll-top.js
@@ -1,13 +1,13 @@
 (() => {
   'use strict'
 
-  const btn = document.getElementById('go-top')
+  const btn = document.getElementById("go-top")
   if (!btn) return
   const fraction = document.currentScript.getAttribute("data-fraction")
-  document.addEventListener('scroll', () => {
+  document.addEventListener("scroll", () => {
     const scrolled = document.documentElement.scrollTop
     const bodySize = document.body.scrollHeight
-    btn.classList.toggle('active', scrolled > bodySize / (fraction ?? 5))
+    btn.classList.toggle("active", scrolled > bodySize / (fraction ?? 5))
   })
-  btn.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }))
+  btn.addEventListener("click", () => window.scrollTo({ top: 0, behavior: "smooth" }))
 })()
